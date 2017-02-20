9ab17eb0adcbf8213e90fb2770676e1516a66ea3
Christian Zufferey <christian@zufferey.com>
2017-02-20 18:49:09 +0100

    Ajout du zdyndns, mais cela ne marche pas encore car les machines ont toutes le même hostname !

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #Petit script pour installer des machines virtuelles Ubuntu dans l'infra de tests Proxmox de la SDF
-#zf170220.1837
+#zf170220.1843
 
-
+sudo apt-get update
+sudo apt-get -y install htop
 
 
 echo "end"
