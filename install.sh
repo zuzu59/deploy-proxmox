89d217bc44cbaada77d0fa9ab3ac39d0ea58752c
Christian Zufferey <christian@zufferey.com>
2017-02-21 15:05:17 +0100

    - modifié le script install.sh afin qu'il tienne compte de l'argument (machine_name) - injecté le machine_name dans le fichier zhost.sh dans /root - injecté le hostname dans /etc/hosts

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 #Petit script pour installer des machines virtuelles Ubuntu dans l'infra de tests Proxmox de la SDF
-#zf170221.0732
+#zf170221.1424
+
+
+#test si l'argument est vide
+if [ -z "$1" ]
+  then
+    echo -e "\nSyntax: install.sh machine_name\n\n"
+    exit
+fi
 
 # modifie au vol le hostname avec l'adrs ip du dhcp
 sudo cp zhost.sh /root
+sudo sed -i "s/THEHOST=\"toto\"/THEHOST=\"$1\"/" /root/zhost.sh
+
+
 sudo crontab root-crontab
 
 # installation des utilitaires
--- zhost.sh
+++ zhost.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 #Petit script pour changer le hostname avec l'adresse ip du dhcp
-#zf170221.0019
+#zf170221.1424
+
+/bin/sleep 3
 
 THEIP=$(/sbin/ifconfig ens18 | /bin/grep "inet ad" | /usr/bin/cut -f2 -d: | /usr/bin/awk '{print $1}')
-/bin/hostname $(echo $THEIP |/bin/sed "s/\./-/g")
+THEHOST="toto"
+
+echo $THEHOST-$THEIP > /dev/kmsg
+
+/bin/hostname $THEHOST-$(echo $THEIP |/bin/sed "s/\./-/g")
+
+echo "$THEIP $(hostname)" >> /etc/hosts
 
 
 
