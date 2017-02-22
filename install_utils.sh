07544238e91e785233da8c71c69829988f00fd4c
Christian Zufferey <christian@zufferey.com>
2017-02-22 18:39:14 +0100

    - ajouté le fichier alias pour les alias

--- install_utils.sh
+++ install_utils.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #Install des utilitaires
-#zf170222.1618
+#zf170222.1836
 
 echo ---------- install des utilitaires
 
-echo "alias gtree='git log --graph --oneline --decorate --all'" >> ~/.bashrc
+cat alias >> ~/.bashrc
 sudo apt-get update
 sudo apt-get -y install htop git
 
