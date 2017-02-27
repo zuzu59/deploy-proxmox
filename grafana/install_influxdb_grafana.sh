9f3a4e82bb6e07b38d33fe30cfb07abc3807734b
Christian Zufferey <christian@zufferey.com>
2017-02-27 09:49:37 +0100

    - mise à jour du dépôt pour automatiser l'installation d'InfluxDB

--- grafana/install_influxdb_grafana.sh
+++ grafana/install_influxdb_grafana.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 #Installation d'un environnement complet Grafana
-#zf151107.1548
+#zf170223.1600
 
-echo ATTENTION, il faut faire tourner ce script dans le dosier Ubuntu !
+echo "ATTENTION, il faut faire tourner ce script dans le dosier racine Ubuntu !"
+echo "donc: ~/deploy-proxmox/grafana/install_influxdb_grafana.sh"
+echo ""
 #read -p "appuyer une touche pour continuer"
 
 echo ---------- update et upgrade
@@ -10,21 +12,16 @@ sudo apt-get -y update
 #apt-get -y upgrade
 
 echo ---------- install grafana and setup
-wget https://grafanarel.s3.amazonaws.com/builds/grafana_2.5.0_amd64.deb
+wget https://grafanarel.s3.amazonaws.com/builds/grafana_4.1.2-1486989747_amd64.deb
 sudo apt-get -y install adduser libfontconfig
-sudo dpkg -i grafana_2.5.0_amd64.deb
+sudo dpkg -i grafana_4.1.2-1486989747_amd64.deb
 sudo update-rc.d grafana-server defaults 95 10
 
 echo ---------- install influxdb
-wget https://s3.amazonaws.com/influxdb/influxdb_0.9.4.2_amd64.deb
-sudo dpkg -i influxdb_0.9.4.2_amd64.deb
+wget https://dl.influxdata.com/influxdb/releases/influxdb_1.2.0_amd64.deb
+sudo dpkg -i influxdb_1.2.0_amd64.deb
 
-echo ---------- install and setup collectd
-sudo apt-get -y install collectd
-
-sudo cp /vagrant/influxdb.conf /etc/opt/influxdb/influxdb.conf
-sudo cp /vagrant/collectd_graphite.conf /etc/collectd/collectd.conf.d/graphite.conf
+#sudo cp /vagrant/influxdb.conf /etc/opt/influxdb/influxdb.conf
 
 sudo service influxdb restart
-sudo service collectd restart
 sudo service grafana-server restart
