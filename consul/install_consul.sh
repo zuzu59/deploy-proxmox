8908fd70480ba32510d0c2e8569e004ace6fb810
Christian Zufferey <christian@zufferey.com>
2017-02-28 21:54:19 +0100

    - ajouté le script pour l'installation de consul

---
+++ consul/install_consul.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#Installation du discovery service Consul
+#zf170228.2153
+
+echo ATTENTION, il faut faire tourner ce script dans le dosier Ubuntu !
+read -p "appuyer une touche pour continuer"
+
+echo ---------- update
+sudo apt-get -y update > /dev/null
+
+echo ---------- install consul
+wget https://releases.hashicorp.com/consul/0.7.5/consul_0.7.5_linux_amd64.zip
+sudo apt-get -y install unzip
+sudo unzip consul_0.7.5_linux_386.zip -d /usr/local/bin/consul
+echo "PATH=$PATH:/usr/local/bin/consul" >> .bashrc
+
+sudo useradd consul
+sudo addgroup consul consul
+sudo addgroup ubuntu consul
+sudo mkdir /var/consul
+sudo chown consul:consul /var/consul
+sudo chmod 775 /var/consul
+sudo mkdir -p /etc/consul.d/{bootstrap,server,client}
+
+echo ---------- 
+echo ---------- 
+echo ---------- IL FAUT SE RECONNECTER POUR RENDRE VALIDE LE NOUVEAU PATH
+echo ---------- 
+echo ---------- 
+
+
+
+
+
