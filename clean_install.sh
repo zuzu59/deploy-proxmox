05aac09d91fb2f3414f8449c7de9858dca4ee360
Christian Zufferey <christian@zufferey.com>
2017-03-03 15:57:10 +0100

    - corrigé encore une fois ce foutu problème de daly update juste après le boot d'une nouvelle machine

--- clean_install.sh
+++ clean_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #Nettoyage de l'installation
-#zf170223.0833
+#zf170303.1551
 
 echo ---------- clean install
+sudo apt-get -f install               #termine l'installa  si problème dû à l'arrêt du service apt-daily
 sudo apt-get -y autoremove
 sudo apt-get -y clean
 sudo apt-get -y autoclean
--- stop_daily_updates.sh
+++ stop_daily_updates.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #stop la mise à jour du système lors du boot (https://github.com/boxcutter/ubuntu/issues/73)
-#zf170223.0839
+#zf170303.1550
 
 echo ---------- stop daily update
 sudo systemctl stop apt-daily.service # disable run when system boot
 sudo systemctl stop apt-daily.timer   # disable timer run
 sudo dpkg --configure -a              #remet en état si problème dû à l'arrêt du service apt-daily
+sudo apt-get -f install               #termine l'installa  si problème dû à l'arrêt du service apt-daily
+
