05dcb6c7aae7869ef911ba2a34a1ef318d3d154e
Christian Zufferey <christian@zufferey.com>
2017-03-05 12:04:31 +0100

    Update env_a_zuzu.sh
    - ajout de l'éditeur par défaut de git, git config --global core.editor nano

--- env_a_zuzu.sh
+++ env_a_zuzu.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #Petit script pour créer mon environnement git ;-)
-#zf170222.1535
+#zf170305.1203
 
 git config --global user.name "Christian Zufferey"
 git config --global user.email "christian@zufferey.com"
 git config --global credential.helper "cache --timeout=86400"
+git config --global core.editor nano
 
 alias gtree='git log --graph --oneline --decorate --all'
 
