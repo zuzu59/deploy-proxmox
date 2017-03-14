97fdc0c59d0a4d456ec2174a509d2c6347234ad8
Christian Zufferey <christian@zufferey.com>
2017-03-14 14:50:57 +0100

    - jouté dans le script install_nodes.sh l'installation du moniteur de node pour compiler à chaque modification du code

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #Install  nodejs
-#zf170314.1349
+#zf170314.1450
 
 echo ---------- install de nodejs
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
+sudo npm install -g nodemon
 node -v
 npm -v
 
