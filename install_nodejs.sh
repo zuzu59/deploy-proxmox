17601e190609210edeef247a4103786446140be9
Christian Zufferey <christian@zufferey.com>
2017-03-14 13:50:33 +0100

    - enlevé le apt-get update dans le script install_nodejs.sh car il était éxécuté deux fois

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 #Install  nodejs
-#zf170314.1334
+#zf170314.1349
 
 echo ---------- install de nodejs
 
-sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
