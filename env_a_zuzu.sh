a9193ed5e537702203a25815139d260f15de7489
Christian Zufferey <christian@zufferey.com>
2017-03-28 15:53:41 +0200

    - ajouté la commande gcr pour vider le cache credential de git - mis à 300 secondes le git credential cache dans les env à zuzu

--- env_a_zuzu.sh
+++ env_a_zuzu.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #Petit script pour créer mon environnement git ;-)
-#zf170305.1203
+#zf170328.1550
 
 git config --global user.name "Christian Zufferey"
 git config --global user.email "christian@zufferey.com"
-git config --global credential.helper "cache --timeout=86400"
+git config --global credential.helper "cache --timeout=300"
 git config --global core.editor nano
 
 alias gtree='git log --graph --oneline --decorate --all'
