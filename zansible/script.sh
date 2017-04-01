6651088cda7e429efe9d92e206fdb3432701e6a0
Christian Zufferey <christian@zufferey.com>
2017-04-02 00:10:47 +0200

    - ajouté la détection de besoin d'upgrade et de reboot dans le script.sh pour les maintenances groupées

--- zansible/script.sh
+++ zansible/script.sh
@@ -4,12 +4,18 @@
 
 echo ---------- script start $1
 
-git -C ./deploy-proxmox/ pull
+#git -C ./deploy-proxmox/ pull
+
 #./deploy-proxmox/patch_sshd_AcceptEnv.sh
 #cat /etc/ssh/sshd_config |grep GIT
 
 #cd ./deploy-proxmox
 #./change_hostname.sh $1
 #sudo /root/zhost.sh
-cat /etc/hosts
+#cat /etc/hosts
+
+#sudo /etc/update-motd.d/90-updates-available
+sudo /etc/update-motd.d/98-reboot-required
+
+
 
