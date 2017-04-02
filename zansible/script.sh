d351d92cc9166682c3f53b7a9961da0ca382cac4
Christian Zufferey <christian@zufferey.com>
2017-04-02 22:33:45 +0200

    - amélioré le filtre pour ne garder que les machines actives pour la listes des machines.txt de zansible - amélioré le script script.sh pour corriger le bug du zhost et de .baschrc - fait les mises à jours de toutes les machines de l'infra avec zansible

--- zansible/script.sh
+++ zansible/script.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 #script.sh, petit script à exécuter en remote
-#zf170331.0907
+#zf170402.1544
 
 echo ---------- script start $1
 
+#### met à jour la distrib deploy-proxmox de github
 #git -C ./deploy-proxmox/ pull
 
+#### patch pour autoriser de transmettre via le ssh forward les variables env pour GIT
 #./deploy-proxmox/patch_sshd_AcceptEnv.sh
 #cat /etc/ssh/sshd_config |grep GIT
 
+#### patch le problème du /etc/hosts
 #cd ./deploy-proxmox
 #./change_hostname.sh $1
 #sudo /root/zhost.sh
 #cat /etc/hosts
 
-
+#### patch le .bashrc
+#cat ~/.bashrc
+#~/deploy-proxmox/install_utils.sh
 
 #### voir si on a besoin de faire des updates
 sudo /etc/update-motd.d/90-updates-available
@@ -26,8 +31,8 @@ sudo /etc/update-motd.d/90-updates-available
 sudo /etc/update-motd.d/98-reboot-required
 
 #### rebooter si nécesssaire
-#ifrestart=`sudo /etc/update-motd.d/98-reboot-required | awk {'print $3'}`
-#if [ "$ifrestart" = "restart" ] ; then echo $ifrestart ; sudo reboot ; fi
+ifrestart=`sudo /etc/update-motd.d/98-reboot-required | awk {'print $3'}`
+if [ "$ifrestart" = "restart" ] ; then echo $ifrestart ; sudo reboot ; fi
 
 #### reboote la machine
 #sudo reboot
