35b43a5d7d356f4f607c91ef04cc5a007c4bc930
Christian Zufferey <christian@zufferey.com>
2017-04-08 12:08:06 +0200

    - modifié zdyndns.sh afin de transmettre aussi l'adress ip privée de la machine

--- zdyndns_client.sh
+++ zdyndns_client.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #Petit client bash pour le zDynDNS
-#zf170221.1701
+#zf170408.1157
 
 while true
 do
 	date
+	THEIP=$(/sbin/ifconfig ens18 | /bin/grep "inet ad" | /usr/bin/cut -f2 -d: | /usr/bin/awk '{print $1}')
 	ZSTR=$(hostname)
-	echo "s "$ZSTR" xyz" | netcat sdftests.epfl.ch 3318
+	echo "s "$ZSTR" "$THEIP | netcat sdftests.epfl.ch 3318
 	sleep $[ ( $RANDOM % 5 ) + 10 ]s
 done
 
