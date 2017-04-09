0c05a6b05b4dccaa5a846db3651300944cb6f1bd
Christian Zufferey <christian@zufferey.com>
2017-04-09 19:24:26 +0200

    - ajouté la commande zdynssh au fichier zfunctions.sh afin de pouvoir faire un ssh via un tremplin sourcé par zdyndns

--- zfunctions.sh
+++ zfunctions.sh
@@ -1,10 +1,17 @@
 #liste des fonctions bash personnelles
-#zf170320.1119
+#zf170409.1923
 
 zdyn() {
 	echo d | nc sdftests.epfl.ch 3318 | grep "$1" | sort
 }
 zdynw() {
 	watch -n 1 "echo d | nc sdftests.epfl.ch 3318 | grep '$1' | sort"
+}
+
+zdynssh() {
+ZDYNDNS=`echo d | nc sdftests.epfl.ch 3318 | grep "$1"`
+IPPUB=`echo $ZDYNDNS | awk {'print $2'}`
+IPPRIV=`echo $ZDYNDNS | awk {'print $3'}`
+ssh -A -t root@$IPPUB ssh -A ubuntu@$IPPRIV
 }
 
