b7430f9cb69be115bc601e65cd84cc66f90da30b
Christian Zufferey <christian@zufferey.com>
2017-05-15 09:19:38 +0200

    - amélioré encore la sortie du script showsdfrp.sh

--- showsdfrp.sh
+++ showsdfrp.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #Petit script pour afficher la config du reverse proxy pour un service via un curl dans Consul
-#zf170515.0913
+#zf170515.0921
 
 
-curl -X GET http://10.92.103.53:8500/v1/catalog/services |jq "" |grep -i $1
+curl -X GET http://10.92.103.53:8500/v1/catalog/services |jq "" |grep "Host:" |grep -i $1
 
 
