2573d14703991cb90f894e0787f758ab6f3dd187
Christian Zufferey <christian@zufferey.com>
2017-05-15 09:10:58 +0200

    - amélioré le script showsdfrp.sh en lui donnant la possibilité de filtrer la sortie sur un argument

--- showsdfrp.sh
+++ showsdfrp.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #Petit script pour afficher la config du reverse proxy pour un service via un curl dans Consul
-#zf170511.1942
+#zf170515.0913
 
 
-curl -X GET http://10.92.103.53:8500/v1/catalog/services |jq
+curl -X GET http://10.92.103.53:8500/v1/catalog/services |jq "" |grep -i $1
+
 
