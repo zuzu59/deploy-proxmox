bc07210c31f4539493d4e0c4b2da827ce82f2635
Christian Zufferey <christian@zufferey.com>
2017-05-18 15:40:25 +0200

    - mis à jour le script d'install du zdyndns afin d'être en absolu

--- install_zdyndns.sh
+++ install_zdyndns.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Installation de zdyndns
-#zf170502.0853
+#zf170518.1540
 
 echo ---------- install zdyndns
 
-sudo cp zdyndns_client.sh /root
-sudo cp zdyndns.service /etc/systemd/system
+sudo cp /home/ubuntu/deploy-proxmox/zdyndns_client.sh /root
+sudo cp /home/ubuntu/deploy-proxmox/zdyndns.service /etc/systemd/system
 sudo chown root:root /etc/systemd/system/zdyndns.service
 sudo systemctl daemon-reload
 sudo systemctl enable zdyndns.service
--- zansible/script.sh
+++ zansible/script.sh
@@ -5,7 +5,7 @@
 echo ---------- script start $1
 
 #### met à jour la distrib deploy-proxmox de github
-#git -C ./deploy-proxmox/ pull
+git -C ./deploy-proxmox/ pull
 
 #### patch pour autoriser de transmettre via le ssh forward les variables env pour GIT
 #./deploy-proxmox/patch_sshd_AcceptEnv.sh
@@ -21,17 +21,24 @@ echo ---------- script start $1
 #cat ~/.bashrc
 #~/deploy-proxmox/install_utils.sh
 
+#### patch le zdyndns
+~/deploy-proxmox/install_zdyndns.sh
+
 #### voir si on a besoin de faire des updates
 #sudo /etc/update-motd.d/90-updates-available
 
 #### fait tous les updates
-#sudo apt-get -y update ; sudo apt-get -y dist-upgrade ; ~/deploy-proxmox/clean_install.sh
+#sudo apt-get -y update ; sudo apt-get -y dist-upgrade ; sudo ~/deploy-proxmox/clean_install.sh
+
+#### clean les anciens updates
+#sudo ~/deploy-proxmox/clean_install.sh
 
 #### voir si on a besoin de rebooter
-sudo /etc/update-motd.d/98-reboot-required
+#sudo /etc/update-motd.d/98-reboot-required
 
 #### rebooter si nécesssaire
 #ifrestart=`sudo /etc/update-motd.d/98-reboot-required | awk {'print $3'}`
+#echo $ifrestart
 #if [ "$ifrestart" = "restart" ] ; then echo $ifrestart ; sudo reboot ; fi
 
 #### reboote la machine
