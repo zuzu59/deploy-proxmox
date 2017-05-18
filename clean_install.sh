a43d3e4a2bdcf38e5700063137848816a2052af3
Christian Zufferey <christian@zufferey.com>
2017-05-18 16:39:55 +0200

    - amélioré le script de nettoyage des installs quand il ne reste presque plus de plus sur le disque

--- clean_install.sh
+++ clean_install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #Nettoyage de l'installation
-#zf170303.1551
+#zf170518.1642
 
 echo ---------- clean install
-sudo apt-get -f install               #termine l'installa  si problème dû à l'arrêt du service apt-daily
-sudo apt-get -y autoremove
 sudo apt-get -y clean
 sudo apt-get -y autoclean
+sudo apt-get -y autoremove
+sudo apt-get -f install               #termine l'installa  si problème dû à l'arrêt du service apt-daily
+sudo apt-get -y clean
+sudo apt-get -y autoclean
+sudo apt-get -y autoremove
 
 
 
