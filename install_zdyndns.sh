5fb02161cfa22398185e0eab21a0df3cdeb31abf
Christian Zufferey <christian@zufferey.com>
2017-05-18 15:44:21 +0200

    - corrigé install_zdyndns.sh pour le restart du service

--- install_zdyndns.sh
+++ install_zdyndns.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Installation de zdyndns
-#zf170518.1540
+#zf170518.1546
 
 echo ---------- install zdyndns
 
-sudo cp /home/ubuntu/deploy-proxmox/zdyndns_client.sh /root
-sudo cp /home/ubuntu/deploy-proxmox/zdyndns.service /etc/systemd/system
+sudo cp ~/deploy-proxmox/zdyndns_client.sh /root
+sudo cp ~/deploy-proxmox/zdyndns.service /etc/systemd/system
 sudo chown root:root /etc/systemd/system/zdyndns.service
 sudo systemctl daemon-reload
 sudo systemctl enable zdyndns.service
-sudo systemctl start zdyndns.service
+sudo systemctl restart zdyndns.service
