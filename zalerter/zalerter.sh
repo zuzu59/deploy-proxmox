d4087f8a6df6accad197eeb1f28d95b5684dff3a
Christian Zufferey <christian@zufferey.com>
2017-05-22 11:56:36 +0200

    - amélioré la sortie du résultat sur une ligne

--- zalerter/zalerter.sh
+++ zalerter/zalerter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #zalerter, Le Nagios du pauvre, petit script tout simple qui permet d'envoyer des messages d'alertes sur Telegram en fonction de 'recettes' simples 
 #qui tournent en remote sur les machines qui se trouvent dans le fichier machines.txt
-#zf170519.1528
+#zf170522.1156
 
 echo ---------- zalerter start
 sshoptions="-q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
@@ -14,7 +14,7 @@ password="password"
 
 filelines=`cat machines.txt`
 for m in $filelines ; do
-	echo ---------- start on $m
+	echo -e $m": \c"
 	scp $sshoptions ./df_all.sh ubuntu@$m:.
 	echo $password | ssh -t -t $sshoptions ubuntu@$m "~/df_all.sh $m" |grep -v $password
 	ssh $sshoptions ubuntu@$m 'rm ~/df_all.sh'
