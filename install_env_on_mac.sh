d846de2824a0e3469f09b73ca58e5a02255783cb
Christian Zufferey <christian@zufferey.com>
2017-07-01 09:56:13 +0200

    - ajouté le script install_env_on_mac.sh pour installer mes env pour GIT facilement sur un MAC

---
+++ install_env_on_mac.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#Install mes environnement sur MAC
+#zf170701.0954
+
+echo ---------- install des utilitaires
+
+echo "source ~/deploy-proxmox/alias" >> ~/.bashrc
+echo "source ~/deploy-proxmox/zfunctions.sh" >> ~/.bashrc
+
+./env_a_zuzu.sh
+
+
+
