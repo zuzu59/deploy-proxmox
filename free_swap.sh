593fc2ada5d01a084aeae8290cbd2c6af9253940
Christian Zufferey <christian@zufferey.com>
2017-08-18 08:25:00 +0200

    - bien amélioré le script pour libérer le swap afin d'améliorer la réactivité de la machine

--- free_swap.sh
+++ free_swap.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+# petit script pour purger le swap afin d'améliorer la réactivité de la machine
+# ATTENTION: cela ne peut tourner seulement si on a assez de RAM disponible !
+#zf170818.0822
 
+#Source: https://askubuntu.com/questions/1357/how-to-empty-swap-if-there-is-free-ram
+
+echo "Etat de la mémoire avant:"
+free
+
+#purge le cache disque
+sync; sync; sync;
+sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
+
+echo "Etat de la mémoire après le purge du cache disque:"
+free
+
+
+#purge le swap si assez de RAM disponible
 free_data="$(free)"
 mem_data="$(echo "$free_data" | grep 'Mem:')"
 free_mem="$(echo "$mem_data" | awk '{print $4}')"
@@ -20,4 +37,6 @@ else
     exit 1
 fi
 
+echo "Etat de la mémoire après le purge du swap:"
+free
 
