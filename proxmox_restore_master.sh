8a053a27938f264075d46a955410bdc3b0b9d881
Christian Zufferey <christian@zufferey.com>
2017-09-08 16:56:37 +0200

    - mis à jour le script pour le nouveau master

--- proxmox_restore_master.sh
+++ proxmox_restore_master.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # petit script pour récupérer le DVD original Ubuntu et le backup du master
-#zf170907.1546
+#zf170908.1437
 
 #Source: 
 
 scp root@128.178.116.29:/var/lib/vz/template/iso/ubuntu-16.04.1-server-amd64.iso /var/lib/vz/template/iso/
-scp root@128.178.116.29:/var/lib/vz/dump/vzdump-qemu-112-2017_09_07-15_41_58.vma.lzo .
+scp root@128.178.116.29:/var/lib/vz/dump/vzdump-qemu-118-2017_09_08-16_43_49.vma.lzo /var/lib/vz/dump/
 
