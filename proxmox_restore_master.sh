317e5ee3056240ecb4dfec7c61af4c15ffc19d04
Christian Zufferey <christian@zufferey.com>
2017-12-05 17:03:48 +0100

    - actualisé le script proxmox_restore_master.sh afin qu'il utilise maintenant les noms DNS et plus les adresses IP

--- proxmox_restore_master.sh
+++ proxmox_restore_master.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # petit script pour récupérer le DVD original Ubuntu et le backup du master
-#zf170908.1437
+#zf171205.1520
 
 #Source: 
 
-scp root@128.178.116.29:/var/lib/vz/template/iso/ubuntu-16.04.1-server-amd64.iso /var/lib/vz/template/iso/
-scp root@128.178.116.29:/var/lib/vz/dump/vzdump-qemu-118-2017_09_08-16_43_49.vma.lzo /var/lib/vz/dump/
+scp root@sdfproxmox1:/var/lib/vz/template/iso/ubuntu-16.04.1-server-amd64.iso /var/lib/vz/template/iso/
+scp root@sdfproxmox1:/var/lib/vz/dump/vzdump-qemu-120-2017_12_05-15_22_26.vma.lzo /var/lib/vz/dump/
 
