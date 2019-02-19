0adb71da932d8320bdb879f2e36a9973e0abf246
Christian Zufferey <christian@zufferey.com>
2019-02-19 17:52:38 +0100

    Ajouté les tabultions à 4 spaces pour Nano et corrigé une faute de frappe pour le viewer d'images

--- install_utils.sh
+++ install_utils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Install des utilitaires
-#zf170908.1704, zf19013.1437
+#zf170908.1704, zf190219.1750
 
 echo ---------- install des utilitaires
 
@@ -8,7 +8,10 @@ echo "source ~/deploy-proxmox/alias" >> ~/.bashrc
 echo "source ~/deploy-proxmox/zfunctions.sh" >> ~/.bashrc
 
 sudo apt-get update
-sudo apt-get -y install htop git unzip jq sudo curl nano hfw
+sudo apt-get -y install htop git unzip jq sudo curl nano feh
+
+echo "set tabstospaces" >> ~/.nanorc
+echo "set tabsize 4" >> ~/.nanorc
 
 
 
