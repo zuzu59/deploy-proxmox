ae27a594759ef332824ed7488c6acf11d0b036fa
Christian Zufferey <christian@zufferey.com>
2019-06-24 15:39:32 +0200

    wip

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Installation de Ansible
-#zf190624.1447
+#zf190624.1538
 
 echo "---------- install de Ansible"
 
@@ -8,4 +8,5 @@ echo "---------- install de Ansible"
 sudo apt update
 sudo apt install -y python3 python3-pip hiera-eyaml
 sudo -H pip3 install ansible bcrypt
+sudo gem install hiera-eyaml
 
