5d04ada76aff87d03bcc79387bacbab2835bf0b1
Christian Zufferey <christian@zufferey.com>
2019-06-26 15:57:29 +0200

    Ajouté graphiz aux utilitaires

--- install_utils.sh
+++ install_utils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Install des utilitaires
-#zf170908.1704, zf190219.1750
+#zf170908.1704, zf190626.1553
 
 echo ---------- install des utilitaires
 
@@ -8,7 +8,7 @@ echo "source ~/deploy-proxmox/alias" >> ~/.bashrc
 echo "source ~/deploy-proxmox/zfunctions.sh" >> ~/.bashrc
 
 sudo apt-get update
-sudo apt-get -y install htop git unzip jq sudo curl nano feh
+sudo apt-get -y install htop git unzip jq sudo curl nano feh graphviz
 
 echo "set tabstospaces" >> ~/.nanorc
 echo "set tabsize 4" >> ~/.nanorc
