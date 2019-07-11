7f856947fc456b4d0a42874bc62675878d408590
Christian Zufferey <christian@zufferey.com>
2019-07-11 10:30:44 +0200

    Ajout de l'installation du plugin pour Openshift dans Ansible

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Installation de Ansible
-#zf190624.1538
+#zf190711.1030
 
 echo "---------- install de Ansible"
 
@@ -9,4 +9,7 @@ sudo apt update
 sudo apt install -y python3 python3-pip hiera-eyaml
 sudo -H pip3 install ansible bcrypt
 sudo gem install hiera-eyaml
+
+#installation du plugin pour openshift
+ansible-galaxy install epfl_idevelop.ansible_module_openshift
 
