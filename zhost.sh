7a17fd5e5d0086fdf15d7c70b3558ba4cb80860e
Christian Zufferey <christian@zufferey.com>
2019-10-10 11:43:09 +0200

    Modifié le script zhost.sh pour le changement du host name

--- zhost.sh
+++ zhost.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #Petit script pour changer le hostname avec l'adresse ip du dhcp, forcer le bon DNS et ajouter la machine dans le DNS
-#zf171122.1619
+#zf171122.1619, zf191010.1140
 
 #THEIP=$(/sbin/ifconfig ens18 | /bin/grep "inet ad" | /usr/bin/cut -f2 -d: | /usr/bin/awk '{print $1}')
-THEIP=$(/sbin/ifconfig | /bin/grep "inet addr" | /bin/grep -v 127\.0 | /usr/bin/cut -f2 -d: | /usr/bin/awk '{print $1}')
+#THEIP=$(/sbin/ifconfig | /bin/grep "inet addr" | /bin/grep -v 127\.0 | /usr/bin/cut -f2 -d: | /usr/bin/awk '{print $1}')
+THEIP=127.0.0.1
 
 THEHOST="toto"
 
@@ -21,6 +22,10 @@ echo "ff02::1 ip6-allnodes" >> /etc/hosts
 echo "ff02::2 ip6-allrouters" >> /etc/hosts
 
 echo "$THEIP $(hostname)" >> /etc/hosts
+
+exit
+
+
 
 # force le bon DNS et interdit la modification par le DHCP !
 chattr -i /etc/resolv.conf
