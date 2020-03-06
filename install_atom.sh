e9cbc72c85086122ecf179e88e3e161041a6b3d5
Christian Zufferey <christian@zufferey.com>
2020-03-06 11:20:56 +0100

    Pris la nouvelle façon d'installer Atom editor

--- install_atom.sh
+++ install_atom.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 #Installation de l'éditeur Atom
-#zf190624.1359
+#zf200306.1116
 
 echo "---------- install d'Atom"
 
 
-sudo add-apt-repository ppa:webupd8team/atom
+#sudo add-apt-repository ppa:webupd8team/atom
+#sudo add-apt-repository ppa:atom.io
+
+
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update
-sudo apt-get install atom
+sudo apt-get install atom g++ build-essential
 
 apm stars --user zuzu59 --install
 
