cdb91880f813c937137cba92f4eefba9098b0a1d
Christian Zufferey <christian@zufferey.com>
2020-03-20 08:57:28 +0100

    Ajouté le script pour démarrer facilement le serveur x11vnc

--- start_vnc.sh
+++ start_vnc.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 #lance le vnc server pour pour voir utiliser le desktop graphique utilisable via VNC
-#zf171111.2055
+#zf200315.1656
 
 echo ---------- start vnc server
 
-sudo x11vnc -passwd tototutu -auth guess -display :0
+#sudo x11vnc -passwd tototutu -auth guess -display :0
+
+x11vnc -loop -shared -passwd toto123
+
