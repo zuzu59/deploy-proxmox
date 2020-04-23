eb231da973ff9b79507d9758d36898c03f3e5994
Christian Zufferey <christian@zufferey.com>
2020-04-23 11:02:05 +0200

    Mis à jour mes alias pour git ;-)

--- install_env_on_mac.sh
+++ install_env_on_mac.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 #Install mes environnement sur MAC
-#zf170701.0954
+#zf200311.1725
+
+# rm ~/.bashrc ~/.bash_profile ~/.nanorc
 
 echo ---------- install des utilitaires
 
 echo "source ~/deploy-proxmox/alias" >> ~/.bashrc
 echo "source ~/deploy-proxmox/zfunctions.sh" >> ~/.bashrc
 
-./env_a_zuzu.sh
+echo "export GIT_AUTHOR_NAME=\"Christian Zufferey\"" >> ~/.bashrc
+echo "export GIT_AUTHOR_EMAIL=\"christian@zufferey.com\"" >> ~/.bashrc
+echo "export GIT_COMMITTER_NAME=\"Christian Zufferey\"" >> ~/.bashrc
+echo "export GIT_COMMITTER_EMAIL=\"christian@zufferey.com\"" >> ~/.bashrc
 
+echo "export EDITOR=nano" >> ~/.bashrc
+echo "export LANG=en_US.UTF-8" >> ~/.bashrc
 
+~/deploy-proxmox/env_a_zuzu.sh
+
+echo "source ~/.bashrc" >> ~/.bash_profile
+
+echo "set tabstospaces" >> ~/.nanorc
+echo "set tabsize 4" >> ~/.nanorc
 
