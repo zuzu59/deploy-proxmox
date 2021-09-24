5d8343015e553cea5075c5ab253cd5db24fe4807
Christian Zufferey <christian@zufferey.com>
2021-09-24 12:45:05 +0200

    add: ajouté les clefs ssh des nouveaux apprentis

--- get_ssh_key_fsd.sh
+++ get_ssh_key_fsd.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 #Petit script pour récupérer toutes les clefs ssh de la FSD sur Github ;-)
-#zf210421.1533
+#zf210421.1533, zf210924.1103
 
 curl https://github.com/zuzu59.keys >> ~/.ssh/authorized_keys
 curl https://github.com/domq.keys >> ~/.ssh/authorized_keys
-curl https://github.com/GregLeBarbar.keys >> ~/.ssh/authorized_keys
+#curl https://github.com/GregLeBarbar.keys >> ~/.ssh/authorized_keys
 curl https://github.com/jdelasoie.keys >> ~/.ssh/authorized_keys
 curl https://github.com/lvenries.keys >> ~/.ssh/authorized_keys
 curl https://github.com/multiscan.keys >> ~/.ssh/authorized_keys
-curl https://github.com/nicolasreymond.keys >> ~/.ssh/authorized_keys
 curl https://github.com/ponsfrilus.keys >> ~/.ssh/authorized_keys
+
+#curl https://github.com/nicolasreymond.keys >> ~/.ssh/authorized_keys
+#curl https://github.com/strikerpt.keys >> ~/.ssh/authorized_keys
+
+curl https://github.com/JaavLex.keys >> ~/.ssh/authorized_keys
 curl https://github.com/SaphireVert.keys >> ~/.ssh/authorized_keys
-curl https://github.com/strikerpt.keys >> ~/.ssh/authorized_keys
-curl https://github.com/TacticsCH.keys >> ~/.ssh/authorized_keys
+curl https://github.com/D4rkHeart.keys >> ~/.ssh/authorized_keys
+curl https://github.com/Azecko.keys >> ~/.ssh/authorized_keys
+curl https://github.com/crazylady2004.keys >> ~/.ssh/authorized_keys
 
 sort -u -o ~/.ssh/authorized_keys ~/.ssh/authorized_keys
 
