df280ab3a0852869cb65ced6c4a2ce5af4fef7d4
Christian Zufferey <christian@zufferey.com>
2023-03-07 15:17:54 +0100

    new: ajouté les deux scripts pour le VPN Wireguard

---
+++ wg_off.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# petit script éteindre le VPN Wireguard
+# zf230307.1140
+
+sudo wg-quick down wg0
+
+
+
---
+++ wg_on.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# petit script allumer le VPN Wireguard
+# zf230307.1140
+
+sudo wg-quick up wg0
+
+
+
