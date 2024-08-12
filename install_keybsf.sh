1a271723faffb206d2e5acdeae330bad75b4a805
root <root@ubuntu1.zuzu59.ch>
2024-08-12 12:23:23 +0000

    refract: j'ai ajouté la question reboot à la fin

--- install_keybsf.sh
+++ install_keybsf.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 #Installation du keybsf sur Linux
-#zf20240812.1415
+#zf20240812.1422
 
 echo -e "LANG=fr_CH.UTF-8\nLANGUAGE=fr_CH\nLC_ALL=fr_CH.UTF-8" | tee /etc/default/locale
-sed -i 's/^# *\(fr_CH.UTF-8\)/\1/' /etc/locale.gen ; locale-gen ; reboot
+sed -i 's/^# *\(fr_CH.UTF-8\)/\1/' /etc/locale.gen ; locale-gen
+
+echo ""
+read -p "Il faut redémarrer pour que cela soit utilisable !"
+read -p "Etes-vous certains ?"
+echo ""
+
+reboot
 
 
