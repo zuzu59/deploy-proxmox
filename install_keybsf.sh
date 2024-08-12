ecc384c056271382fd24bf943c181dc5c2f172b7
root <root@ubuntu1.zuzu59.ch>
2024-08-12 12:39:11 +0000

    new: ajouté la ligne magique pour pouvoir l'installer sans devoir tout descendre deployproxmox

--- install_keybsf.sh
+++ install_keybsf.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 #Installation du keybsf sur Linux
-#zf20240812.1422
+#zf20240812.1439
+
+
+# pour pouvoir l'installer en seulement une ligne copier/coller
+# wget -qO- https://raw.githubusercontent.com/zuzu59/deploy-proxmox/master/install_keybsf.sh > /tmp/install_keybsf.sh && chmod +x /tmp/install_keybsf.sh && /tmp/install_keybsf.sh
+
+
+
 
 echo -e "LANG=fr_CH.UTF-8\nLANGUAGE=fr_CH\nLC_ALL=fr_CH.UTF-8" | tee /etc/default/locale
 sed -i 's/^# *\(fr_CH.UTF-8\)/\1/' /etc/locale.gen ; locale-gen
