2ee13f1faab574c76a1bb6ffe966b6f79e06555e
Christian Zufferey <christian@zufferey.com>
2025-01-14 11:08:24 +0100

    update: mis à jour le script d'installation de Docker

--- install_docker.sh
+++ install_docker.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 #Petit script pour installer facilement Docker et ses outils qui vont bien
-#zf210822.1928
+#zf210822.1928, zf250107.1729
 
 # source: https://doc.ubuntu-fr.org/docker
 # source: https://docs.docker.com/compose/install/#install-compose
 
 echo -e "\Installation de docker..."
 sudo apt-get update
-sudo apt-get -y install python2-minimal python3-minimal
+sudo apt-get -y install python2-minimal
+sudo apt-get -y install python3-minimal curl htop tree
 #./install_docker.sh
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 sudo usermod -aG docker $LOGNAME
 
-echo -e "\nInstallation de docker-compose..."
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+#echo -e "\nInstallation de docker-compose..."
+##sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#docker-compose --version
 
 echo -e "\nIMPORTANT !\n\nVous devez faire un logoff/logon pour que les modifications de groups fonctionnent !\n"
+
+
