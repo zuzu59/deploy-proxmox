40b2bbc6f9cd8a860b5a76af66501d7449fbeabc
Christian Zufferey <christian@zufferey.com>
2025-04-15 15:43:05 +0200

    refact: refactorisé le script install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 #Petit script pour installer facilement Docker et ses outils qui vont bien
-#zf210822.1928, zf250107.1729
+#zf210822.1928, zf250415.1541
 
 # source: https://doc.ubuntu-fr.org/docker
 # source: https://docs.docker.com/compose/install/#install-compose
 
 echo -e "\Installation de docker..."
 sudo apt-get update
-sudo apt-get -y install python2-minimal
-sudo apt-get -y install python3-minimal curl htop tree
+sudo apt-get -y install python2-minimal python3-minimal curl htop tree
 #./install_docker.sh
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 sudo usermod -aG docker $LOGNAME
