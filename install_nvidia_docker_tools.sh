f0c240cad061ec9b50f55f40f3cc8cd55f20c90c
Christian Zufferey <christian@zufferey.com>
2025-10-15 18:31:24 +0200

    new: ajouté un script pour installer les nvidia docker tools

---
+++ install_nvidia_docker_tools.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#Petit script pour installer facilement les NVIDIA Docker tools
+#zf251015.1822, zf251015.1823
+
+# source: https://hub.docker.com/r/ollama/ollama
+
+echo -e "\Installation des NVIDIA Docker tools..."
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
+  sudo apt-get install -y \
+      nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
+
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
+
+sudo docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
+
+echo -e "\nVoilà, c'est terminé \o/\n"
+
+
